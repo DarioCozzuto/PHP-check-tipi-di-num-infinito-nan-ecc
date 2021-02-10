f06b2e85ec39f1ad5024130d1d1d37f6dcc1402a
DarioCozzuto <77857380+DarioCozzuto@users.noreply.github.com>
2021-02-10 20:32:23 +0100

    Add files via upload

---
+++ PHP check tipi di num infinito nan ecc.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+$x=10; 
+$y=19.2;
+$z="234"; 
+
+//convertire float to int
+$variabile=int($y);
+
+//convertire int to float
+
+
+//convertire stringa to int
+
+
+
+
+?>
+ 
+</body>
+</html>
